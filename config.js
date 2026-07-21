cf7f727a817d49e00c96c627aff35a4267c77112
Jessie Rodriguez <Rjessie760@icloud.com>
2026-07-21 01:30:50 -0700

    Connect TaskCore website to Render API

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const TASKCORE_BOOKING_URL = "";
 const TASKCORE_WEBSITE_URL = "https://mrjgames.github.io/taskcore-booking-page/";
 // Set this to the deployed backend origin, with no trailing slash. Leave blank to use the text-message fallback.
-const TASKCORE_API_URL = "";
+const TASKCORE_API_URL = "https://taskcore-api-rlvr.onrender.com";
